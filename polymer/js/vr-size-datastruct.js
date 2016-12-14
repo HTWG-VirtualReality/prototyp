03be00ca23bed9e2898788e47414dccbc8311d0b
magoeke <magoeke@htwg-konstanz.de>
2016-12-14 14:19:41 +0100

    Added additional datastructure for sizing.

---
+++ polymer/js/vr-size-datastruct.js
@@ -0,0 +1,6 @@
+// datastruct for size
+VrSizeDatastruct = function(min, max, percentage) {
+    function getSizeInformation() {
+        return {"min": min, "max": max, "percentage": percentage};
+    }
+}
