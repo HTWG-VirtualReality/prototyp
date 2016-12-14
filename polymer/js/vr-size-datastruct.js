f41204cb3653c1e960eeab7aa2fee1d18199b468
magoeke <magoeke@htwg-konstanz.de>
2016-12-14 19:59:34 +0100

    Fixed size and dynamic size is possible.

--- polymer/js/vr-size-datastruct.js
+++ polymer/js/vr-size-datastruct.js
@@ -1,37 +1,44 @@
 // datastruct for size
 VrSizeDatastruct = function(min, max, percentage) {
-    return function (width, height) {
-        var result = { width: 0, height: 0};
+    return {
+        calculateSize: function (width, height) {
+            var result = { width: 0, height: 0};
 
-        if(exists(percentage)) { result = calculate(percentage, result, calc(width,height,multiply)); };
-        if(exists(min)) { result = calculate(min, result, comp(greater)); }
-        if(exists(max)) { result = calculate(max, result, comp(smaller)); }
+            if(exists(percentage)) { result = calculate(percentage, result, calc(width,height,multiply)); };
+            if(exists(min)) { result = calculate(min, result, comp(greater)); }
+            if(exists(max)) { result = calculate(max, result, comp(smaller)); }
 
-        return result;
-
-        function calculate(sizeObj, currentResult, func) {
-            var result = {};
-            result["width"] = exists(sizeObj.width) ? func(sizeObj, currentResult, "width") : currentResult.width;
-            result["height"] = exists(sizeObj.height) ? func(sizeObj, currentResult, "height") : currentResult.height;
             return result;
-        }
 
-        function calc(width, height, func) {
-            var operands = {width: width, height: height}
-            return function(a, b, key) {
-                return func(a[key], operands[key]);
+            function calculate(sizeObj, currentResult, func) {
+                var result = {};
+                result["width"] = exists(sizeObj.width) ? func(sizeObj, currentResult, "width") : currentResult.width;
+                result["height"] = exists(sizeObj.height) ? func(sizeObj, currentResult, "height") : currentResult.height;
+                return result;
             }
-        }
 
-        function comp(func) {
-            return function(a, b, key) {
-                return func(a[key], b[key])
+            function calc(width, height, func) {
+                var operands = {width: width, height: height}
+                return function(a, b, key) {
+                    return func(a[key], operands[key]);
+                }
             }
-        }
 
-        function multiply(a,b) { return a*b; }
-        function smaller(a,b) { return (a<b) ? a : b; }
-        function greater(a,b) { return (a>b) ? a : b; }
-        function exists(value) { return value !== null && value !== undefined && value !== {}; }
+            function comp(func) {
+                return function(a, b, key) {
+                    return func(a[key], b[key])
+                }
+            }
+
+            function multiply(a,b) { return a*b; }
+            function smaller(a,b) { return (a<b) ? a : b; }
+            function greater(a,b) { return (a>b) ? a : b; }
+            function exists(value) { return value !== null && value !== undefined && value !== {}; }
+        },
+
+        isAdaptable: function() {
+            return (exists(percentage) || exists(min) || exists(max)) ? false : true;
+            function exists(value) { return value !== null && value !== undefined && value !== {}; }
+        }
     }
 }
