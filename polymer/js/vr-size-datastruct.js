b460b7444cee14c75939950621f743db11c3535c
magoeke <magoeke@htwg-konstanz.de>
2016-12-15 18:03:49 +0100

    resize with minimum value works.

--- polymer/js/vr-size-datastruct.js
+++ polymer/js/vr-size-datastruct.js
@@ -1,43 +1,44 @@
 // datastruct for size
 VrSizeDatastruct = function(min, max, percentage) {
+    // TODO: check if percentage exist or min and max are the same
     return {
-        calculateSize: function (width, height) {
+        calculateSize: function (width, propWidth, height, propHeight) {
             var result = { width: 0, height: 0};
+            console.log(propHeight)
 
-            if(exists(percentage)) { result = calculate(percentage, result, calc(width,height,multiply)); };
-            if(exists(min)) { result = calculate(min, result, comp(greater)); }
-            if(exists(max)) { result = calculate(max, result, comp(smaller)); }
+            result.width = width * (percentage.width * propWidth);
+            result.height = height * (percentage.height * propHeight);
 
-            return result;
+            var diff = calculateDiff(min, result.height);
+            // adjust diff to minimum
+            console.log("diff: " + diff)
+            if(diff < 0) { result.height -= diff; }
 
-            function calculate(sizeObj, currentResult, func) {
-                var result = {};
-                result["width"] = exists(sizeObj.width) ? func(sizeObj, currentResult, "width") : currentResult.width;
-                result["height"] = exists(sizeObj.height) ? func(sizeObj, currentResult, "height") : currentResult.height;
-                return result;
+            return {
+                size: result,
+                diffMinHeight: (diff < 0) ? -(result.height) : 0,
+                percentageHeight:  (diff > 0) ? percentage.height : 0
             }
 
-            function calc(width, height, func) {
-                var operands = {width: width, height: height}
-                return function(a, b, key) {
-                    return func(a[key], operands[key]);
-                }
+            function calculateDiff(obj, height) {
+                return height - ((exists(obj) && exists(obj.height)) ? obj.height : 0);
             }
 
-            function comp(func) {
-                return function(a, b, key) {
-                    return func(a[key], b[key])
-                }
-            }
-
-            function multiply(a,b) { return a*b; }
-            function smaller(a,b) { return (a<b) ? a : b; }
-            function greater(a,b) { return (a>b) ? a : b; }
             function exists(value) { return value !== null && value !== undefined && value !== {}; }
         },
 
         isAdaptable: function() {
             return (exists(percentage) || exists(min) || exists(max)) ? false : true;
+            function exists(value) { return value !== null && value !== undefined && value !== {}; }
+        },
+
+        getMaxHeight: function() {
+            return (exists(max) && exists(max.height)) ? max.height : null;
+            function exists(value) { return value !== null && value !== undefined && value !== {}; }
+        },
+
+        getPercentageHeight: function() {
+            return (exists(percentage) && exists(percentage.height)) ? percentage.height : null;
             function exists(value) { return value !== null && value !== undefined && value !== {}; }
         }
     }
