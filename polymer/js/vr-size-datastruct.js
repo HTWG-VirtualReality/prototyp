22cbcef1b19409086372d31d63bfe8dd96c89933
magoeke <magoeke@htwg-konstanz.de>
2016-12-15 19:57:31 +0100

    Refactored vr-size-datastruct.

--- polymer/js/vr-size-datastruct.js
+++ polymer/js/vr-size-datastruct.js
@@ -2,33 +2,33 @@
 VrSizeDatastruct = function(min, max, percentage) {
     if(!exists(percentage)) {
         console.error("Percentage is required.");
-        return function() { console.error("Percentage is empty."); }; }
+        return function() { console.error("Percentage is empty."); };
+    }
 
     return function (width, propWidth, height, propHeight) {
         var result = { width: 0, height: 0};
 
+        // calculate new sizes
         result.width = width * (percentage.width * propWidth);
         result.height = height * (percentage.height * propHeight);
 
+        // check if new height outruns min or max height
         var diff = calculateDiff(min, max, result.height);
 
-        if(diff.min) { result.height -= diff.value }
-        else if(diff.max) { result.height += diff.value }
+        // adjust height to found diff
+        result.height = handleDiff(diff, result.height);
 
         return {
             size: result,
-            diffMinHeight: (diff.value < 0) ? -(result.height) : 0,
-            percentageHeight:  (diff.value >= 0) ? percentage.height : 0
+            diffHeight: outruns(diff) ? -(result.height) : 0,
+            percentageHeight: outruns(diff) ? 0 : percentage.height
         }
 
+        // calculates Difference between height and min/max height
         function calculateDiff(min, max, height) {
-            var diff = sub(order1, min, height);
-            var min = (diff < 0) ? true : false;
-            diff = (diff < 0) ? diff : sub(order2, max, height);
             return {
-                value: diff,
-                min: min,
-                max: (diff < 0 && !min) ? true : false
+                min: sub(order1, min, height),
+                max: sub(order2, max, height)
             }
         }
 
@@ -37,10 +37,15 @@ VrSizeDatastruct = function(min, max, percentage) {
             return 0;
         }
 
+        function handleDiff(diff, dimension) {
+            if(diff.min < 0) { return dimension -= diff.min; }
+            else if(diff.max < 0) { return dimension += diff.max; }
+            return dimension;
+        }
+
+        function outruns(diff) { return diff.min < 0 || diff.max < 0}
         function order1(a,b) {return a-b;}
         function order2(a,b) {return b-a;}
-
-        // function exists(value) { return value !== null && value !== undefined && value !== {}; }
     };
 
     function exists(value) { return value !== null && value !== undefined && value !== {}; }
